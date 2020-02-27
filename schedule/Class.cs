7d5e26079b818d3c2c805911733ffe3189fffb02
Yarema Stiahar <zapasnyy@gmail.com>
2020-02-27 17:45:51 +0200

    little changes

--- schedule/Class.cs
+++ schedule/Class.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace schedule
 {
+    public enum Day
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+    }
     class Class
     {
         public Subject Subject { get; set; }
         public Group Group { get; set; }
         public Room Room { get; set; }
+        public string Teacher { get; set; }
+        public Day Day { get; set; }
+        public uint Number { get; set; }
     }
 }
