c3ec3a709e50d1fae9a03e153cbea62a030d44ca
Yarema Stiahar <zapasnyy@gmail.com>
2020-02-28 10:30:22 +0200

    check

--- schedule/Subject.cs
+++ schedule/Subject.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace schedule
 {
+    public enum SubjectType
+    {
+        Comuter,
+        Lecture,
+        Ordinary,
+    }
     class Subject
     {
         public string Name { get; set; }
+        public SubjectType SubjectType { get; set; }
 
         public override string ToString()
         {
             return Name;
         }
     }
-
-    class Lecture : Subject
-    {
-
-
-    }
-
 }
