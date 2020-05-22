5d94948d205bcfd29218eaeac71a06d3690f9401
maxSlkk <max.slushch@gmail.com>
2020-05-22 14:28:27 +0300

    make some fixes

--- schedule/DB/ScheduleDbContext.cs
+++ schedule/DB/ScheduleDbContext.cs
@@ -30,7 +30,7 @@ namespace schedule
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseLazyLoadingProxies().UseNpgsql("Host=localhost;Port=5432;Database=scheduledb;Username=postgres;Password=1111");
+                optionsBuilder.UseLazyLoadingProxies().UseNpgsql("Host=localhost;Port=5432;Database=scheduledb;Username=postgres;Password=1234");
             }
         }
 
